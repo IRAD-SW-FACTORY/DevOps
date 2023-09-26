c50ed2b0b502fe2bc4ab7d13dd9fb8ffee693c86
Jorge <jorge@jamtech.cl>
2023-09-25 21:34:01 -0300

    refactor

--- .github/scripts/msbuild/qualitygate.ps1
+++ .github/scripts/msbuild/qualitygate.ps1
@@ -1,6 +1,6 @@
 $SONAR_URL = if ($env:SONAR_URL) { $env:SONAR_URL } else { "http://ec2-54-164-26-4.compute-1.amazonaws.com" };
 $REPO_NAME = if ($env:REPO_NAME) { $env:REPO_NAME } else { "DevOps" };
-$SONAR_TOKEN = if ($env:SONAR_TOKEN) { $env:SONAR_TOKEN } else { "c3FhX2Y1MDVhYTA4YTRhNmJmNzI0MDI0YThmZDBkNTBhN2RiN2RkYWQyNzA6" };
+$SONAR_TOKEN = if ($env:SONAR_TOKEN) { $env:SONAR_TOKEN } else { "-" };
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", "Basic ${SONAR_TOKEN}")
 $response = Invoke-RestMethod "${SONAR_URL}/api/qualitygates/project_status?projectKey=${REPO_NAME}" -Method 'GET' -Headers $headers
@@ -9,7 +9,3 @@ if ($result.projectStatus.status -eq "ERROR") {
     Write-Output $result
     exit 1 
 } 
-else 
-{
-    exit 0
-}
