f18a795d3fd0742cdb1197c74062922caebc365c
Jorge <jorge@jamtech.cl>
2023-09-25 21:50:12 -0300

    r

--- .github/scripts/msbuild/qualitygate.ps1
+++ .github/scripts/msbuild/qualitygate.ps1
@@ -4,7 +4,7 @@ $REPO_NAME = $env:REPO_NAME;
 $SONAR_TOKEN = $env:SONAR_TOKEN;
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", "Basic ${SONAR_TOKEN}")
-$response = Invoke-RestMethod "${SONAR_URL}/api/qualitygates/project_status?projectKey=${REPO_NAME}" -Method 'GET' -Headers $headers
+$response = Invoke-RestMethod "${SONAR_URL}api/qualitygates/project_status?projectKey=${REPO_NAME}" -Method 'GET' -Headers $headers
 $result = $response | ConvertTo-Json | ConvertFrom-Json
 Write-Output $result.projectStatus.status
 if ($result.projectStatus.status -eq "ERROR") { 
