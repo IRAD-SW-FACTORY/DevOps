61dc6e11e2e2a4b3d39ea08e0bb800ef42b0da37
Jorge <jorge@jamtech.cl>
2023-09-25 23:40:07 -0300

    fix

--- .github/scripts/msbuild/qualitygate.ps1
+++ .github/scripts/msbuild/qualitygate.ps1
@@ -1,10 +1,11 @@
 $SONAR_URL = if ($null -ne $env:SONAR_URL) { $env:SONAR_URL } else { "http://ec2-54-164-26-4.compute-1.amazonaws.com/" };
 $REPO_NAME = if ($null -ne $env:REPO_NAME) { $env:REPO_NAME } else { "DevOps" };
 $SONAR_TOKEN = if ($null -ne $env:SONAR_TOKEN) { $env:SONAR_TOKEN } else { "c3FhX2Y1MDVhYTA4YTRhNmJmNzI0MDI0YThmZDBkNTBhN2RiN2RkYWQyNzA6" };
+$URL = "${SONAR_URL}api/qualitygates/project_status?projectKey=${REPO_NAME}"
 Write-Output "Starting sonarqube quality gate status of ${REPO_NAME} on instance ${SONAR_URL}"
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", "Basic ${SONAR_TOKEN}")
-$response = Invoke-RestMethod "${SONAR_URL}api/qualitygates/project_status?projectKey=${REPO_NAME}" -Method 'GET' -Headers $headers
+$response = Invoke-RestMethod $URL -Method "GET" -Headers $headers
 $result = $response | ConvertTo-Json | ConvertFrom-Json
 Write-Output $result
 if ($result.projectStatus.status -eq "OK") { 
@@ -12,4 +13,6 @@ if ($result.projectStatus.status -eq "OK") {
 } 
 else {
     $env:EXIT_CODE = 1;
+    [Environment]::Exit(1)
 }
+[Environment]::Exit(0)
