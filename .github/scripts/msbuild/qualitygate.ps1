39c7df368df38ef12b56177ca26fa8db42418324
Jorge <jorge@jamtech.cl>
2023-09-25 21:39:23 -0300

    fix

--- .github/scripts/msbuild/qualitygate.ps1
+++ .github/scripts/msbuild/qualitygate.ps1
@@ -1,11 +1,13 @@
-$SONAR_URL = if ($env:SONAR_URL) { $env:SONAR_URL } else { "http://ec2-54-164-26-4.compute-1.amazonaws.com" };
-$REPO_NAME = if ($env:REPO_NAME) { $env:REPO_NAME } else { "DevOps" };
-$SONAR_TOKEN = if ($env:SONAR_TOKEN) { $env:SONAR_TOKEN } else { "-" };
+Write-Output "Starting sonarqube quality gate status check"
+$SONAR_URL = $env:SONAR_URL;
+$REPO_NAME = $env:REPO_NAME;
+$SONAR_TOKEN = $env:SONAR_TOKEN;
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", "Basic ${SONAR_TOKEN}")
 $response = Invoke-RestMethod "${SONAR_URL}/api/qualitygates/project_status?projectKey=${REPO_NAME}" -Method 'GET' -Headers $headers
 $result = $response | ConvertTo-Json | ConvertFrom-Json
+Write-Output $result.projectStatus.status
 if ($result.projectStatus.status -eq "ERROR") { 
-    Write-Output $result
     exit 1 
-} 
+}
+exit 0
