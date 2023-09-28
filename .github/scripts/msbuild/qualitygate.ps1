c6218b9c784f69287faad59a07f759ef71aabe1f
Jorge <jorge@jamtech.cl>
2023-09-28 13:38:51 -0300

    refactor

--- .github/scripts/msbuild/qualitygate.ps1
+++ .github/scripts/msbuild/qualitygate.ps1
@@ -1,10 +1,10 @@
 $SONAR_URL = if ($null -ne $env:SONAR_URL) { $env:SONAR_URL } else { "http://ec2-54-164-26-4.compute-1.amazonaws.com/" };
 $REPO_NAME = if ($null -ne $env:REPO_NAME) { $env:REPO_NAME } else { "DevOps" };
-$SONAR_TOKEN = if ($null -ne $env:SONAR_TOKEN) { $env:SONAR_TOKEN } else { "c3FhX2Y1MDVhYTA4YTRhNmJmNzI0MDI0YThmZDBkNTBhN2RiN2RkYWQyNzA6" };
+$SONAR_BASIC_TOKEN = if ($null -ne $env:SONAR_BASIC_TOKEN) { $env:SONAR_BASIC_TOKEN } else { "c3FhX2Y1MDVhYTA4YTRhNmJmNzI0MDI0YThmZDBkNTBhN2RiN2RkYWQyNzA6" };
 $URL = "${SONAR_URL}api/qualitygates/project_status?projectKey=${REPO_NAME}"
 Write-Output "Getting sonarqube quality gate status from ${URL}"
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", "Basic ${SONAR_TOKEN}")
+$headers.Add("Authorization", "Basic ${SONAR_BASIC_TOKEN}")
 $response = Invoke-RestMethod $URL -Method "GET" -Headers $headers
 $result = $response | ConvertTo-Json | ConvertFrom-Json
 Write-Output $result.projectStatus
