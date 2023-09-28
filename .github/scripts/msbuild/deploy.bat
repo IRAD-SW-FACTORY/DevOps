e29eb7ea0245308a4c6c0d33b4c0cd6ccc82f5da
Jorge <jorge@jamtech.cl>
2023-09-28 18:15:35 -0300

    stop iss

--- .github/scripts/msbuild/deploy.bat
+++ .github/scripts/msbuild/deploy.bat
@@ -7,8 +7,12 @@ echo "%APP_NAME%" > %VERSION_FILE%
 echo "%VERSION%" >> %VERSION_FILE%
 net use X: %DESTINATION_PATH% "%DESTINATION_PWD%" /User:%DESTINATION_USER%
 xcopy Deploy\* X:\%APP_NAME%_%RUN_ID%\* /Y /E
+echo "Stop App Pool %APP_NAME%"
+PsExec64.exe --nobanner \\%SERVER% -u %DESTINATION_USER% -p %DESTINATION_PWD% C:\Windows\System32\inetsrv\appcmd stop apppool /apppool.name:"%APP_NAME%%"
 X:
 move /Y X:\%APP_NAME% X:\Backups\%APP_NAME%_%RUN_ID%
 rename X:\%APP_NAME%_%RUN_ID% %APP_NAME%
 c:
 net use X: /d /Y
+echo "Start App Pool %APP_NAME%"
+PsExec64.exe --nobanner \\%SERVER% -u %DESTINATION_USER% -p %DESTINATION_PWD% C:\Windows\System32\inetsrv\appcmd start apppool /apppool.name:"%APP_NAME%%"
