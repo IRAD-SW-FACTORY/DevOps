edce9db96f17e2275c41c7a74ad1084819df88e9
Jorge <jorge@jamtech.cl>
2023-09-28 18:44:35 -0300

    fix

--- .github/scripts/msbuild/deploy.bat
+++ .github/scripts/msbuild/deploy.bat
@@ -8,11 +8,11 @@ echo "%VERSION%" >> %VERSION_FILE%
 net use X: %DESTINATION_PATH% "%DESTINATION_PWD%" /User:%DESTINATION_USER%
 xcopy Deploy\* X:\%APP_NAME%_%RUN_ID%\* /Y /E
 echo "Stop App Pool %APP_NAME%"
-PsExec64.exe --nobanner -accepteula "\\%SERVER%" -u "%DESTINATION_USER%" -p "%DESTINATION_PWD%" "C:\Windows\System32\inetsrv\appcmd.exe stop apppool /apppool.name:%APP_NAME%"
+PsExec64.exe --nobanner -accepteula "\\%SERVER%" -u "%DESTINATION_USER%" -p "%DESTINATION_PWD%" "C:\Windows\System32\inetsrv\appcmd.exe" "stop apppool /apppool.name:%APP_NAME%"
 X:
 move /Y X:\%APP_NAME% X:\Backups\%APP_NAME%_%RUN_ID%
 rename X:\%APP_NAME%_%RUN_ID% %APP_NAME%
 c:
 net use X: /d /Y
 echo "Start App Pool %APP_NAME%"
-PsExec64.exe --nobanner -accepteula "\\%SERVER%"" -u "%DESTINATION_USER%" -p "%DESTINATION_PWD%" "C:\Windows\System32\inetsrv\appcmd.exe start apppool /apppool.name:%APP_NAME%"
+PsExec64.exe --nobanner -accepteula "\\%SERVER%" -u "%DESTINATION_USER%" -p "%DESTINATION_PWD%" "C:\Windows\System32\inetsrv\appcmd.exe" "start apppool /apppool.name:%APP_NAME%"
