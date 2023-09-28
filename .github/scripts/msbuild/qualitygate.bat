4e197deb4cb93869d23f90184f187367ca9d6aa4
Jorge <jorge@jamtech.cl>
2023-09-28 12:21:13 -0300

    fix

--- .github/scripts/msbuild/qualitygate.bat
+++ .github/scripts/msbuild/qualitygate.bat
@@ -3,9 +3,10 @@ rem set SONAR_TOKEN="c3FhX2Y1MDVhYTA4YTRhNmJmNzI0MDI0YThmZDBkNTBhN2RiN2RkYWQyNzA
 rem set SONAR_URL="http://ec2-54-164-26-4.compute-1.amazonaws.com/"
 rem set REPO_NAME="DevOps"
 set url="%SONAR_URL%api/qualitygates/project_status?projectKey=%REPO_NAME%"
+set header="Authorization: Basic %SONAR_TOKEN%"
 echo %url%
-curl -v --header "Authorization: Basic %SONAR_TOKEN%" --location %url% 
-curl -s --header "Authorization: Basic %SONAR_TOKEN%" --location %url% > output.json
+curl -v --header %header% --location %url% 
+curl -s --header %header% --location %url% > output.json
 type output.json | jq .projectStatus.status > status.txt
 set /p Status=<status.txt
 echo Quality gate status: %Status%
