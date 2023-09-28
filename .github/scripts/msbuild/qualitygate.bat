edecbd671652272caa225b1b302c87b30b5fe74a
Jorge <jorge@jamtech.cl>
2023-09-28 12:18:35 -0300

    aa

--- .github/scripts/msbuild/qualitygate.bat
+++ .github/scripts/msbuild/qualitygate.bat
@@ -1,5 +1,5 @@
 @echo off
-set SONAR_TOKEN="c3FhX2Y1MDVhYTA4YTRhNmJmNzI0MDI0YThmZDBkNTBhN2RiN2RkYWQyNzA6"
+rem set SONAR_TOKEN="c3FhX2Y1MDVhYTA4YTRhNmJmNzI0MDI0YThmZDBkNTBhN2RiN2RkYWQyNzA6"
 rem set SONAR_URL="http://ec2-54-164-26-4.compute-1.amazonaws.com/"
 rem set REPO_NAME="DevOps"
 set url="%SONAR_URL%api/qualitygates/project_status?projectKey=%REPO_NAME%"
