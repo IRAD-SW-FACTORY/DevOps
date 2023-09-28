6f52f6bdb65c5a7d74e0ddd92382abbb7eee172e
Jorge <jorge@jamtech.cl>
2023-09-28 14:12:11 -0300

    test

--- .github/scripts/msbuild/analyze.bat
+++ .github/scripts/msbuild/analyze.bat
@@ -4,4 +4,5 @@ SonarScanner.MSBuild.exe begin /k:"%REPO_NAME%" /d:sonar.login="%SONAR_TOKEN%" /
 msbuild /t:restore /p:RestorePackagesConfig=true /p:Configuration=%BUILD_CONFIG% /t:rebuild
 @if "%TESTS_PATH%" == "-" (echo "No tests") else vstest.console %TESTS_PATH%
 SonarScanner.MSBuild.exe end /d:sonar.login="%SONAR_TOKEN%"
+rem Sleep 10 seconds to wait for server to process analysis result
 ping -n 10 127.0.0.1 >NUL
