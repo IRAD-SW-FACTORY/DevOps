97a25a6291f192e7e18c9aa77d4794cf7c0e74b5
Jorge <jorge@jamtech.cl>
2023-09-28 13:12:24 -0300

    w

--- .github/scripts/msbuild/qualitygate.bat
+++ .github/scripts/msbuild/qualitygate.bat
@@ -3,7 +3,7 @@ rem set SONAR_TOKEN="c3FhX2Y1MDVhYTA4YTRhNmJmNzI0MDI0YThmZDBkNTBhN2RiN2RkYWQyNzA
 rem set SONAR_URL="http://ec2-54-164-26-4.compute-1.amazonaws.com/"
 rem set REPO_NAME="DevOps"
 set url="%SONAR_URL%api/qualitygates/project_status?projectKey=%REPO_NAME%"
-set header="Authorization: Basic %SONAR_TOKEN_NEW%"
+set header="Authorization: Basic %SONAR_KEY%"
 echo %url%
 echo %header% > header.txt
 type header.txt
