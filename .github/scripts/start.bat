006dad7099505ccf16e02ef90d4370b701746d6a
Jorge <jaguadom@gmail.com>
2023-09-28 23:56:58 -0300

    Fix start/stop scripts for deployment (#19)

--- .github/scripts/start.bat
+++ .github/scripts/start.bat
@@ -1,13 +1,6 @@
 echo "Start App Pool or Service %APP_NAME%"
-
-Call :strlen %REPO_NAME% _length
-SET KIND=%REPO_NAME%:~_length-6,6"
-echo %KIND%
-echo String is %_length% characters long
-
-
-IF %KIND% == "WinService" GOTO service 
-IF %KIND% == "AppWeb" GOTO website 
+IF "%KIND%" == "WinService" GOTO :service 
+IF "%KIND%" == "AppWeb" GOTO :website 
 
 goto:eof
 
@@ -17,17 +10,3 @@ goto:eof
 
 :website
 PsExec64.exe -s -nobanner -accepteula \\%SERVER% -u %DESTINATION_USER% -p "%DESTINATION_PWD%" "C:\Windows\System32\inetsrv\appcmd.exe" "start apppool /apppool.name:%APP_NAME%"
-
-goto:eof
-:strlen  StrVar  [RtnVar]
-  setlocal EnableDelayedExpansion
-  set "s=#!%~1!"
-  set "len=0"
-  for %%N in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
-    if "!s:~%%N,1!" neq "" (
-      set /a "len+=%%N"
-      set "s=!s:~%%N!"
-    )
-  )
-  endlocal&if "%~2" neq "" (set %~2=%len%) else echo %len%
-exit /b
