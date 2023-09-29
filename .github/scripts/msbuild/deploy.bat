9da3872d63c0520294aeaec7dcfe714f944f688a
Jorge <jaguadom@gmail.com>
2023-09-28 23:48:54 -0300

    Fix paths and deployment for services (#18)

--- .github/scripts/msbuild/deploy.bat
+++ .github/scripts/msbuild/deploy.bat
@@ -7,10 +7,10 @@ echo "%APP_NAME%" > %VERSION_FILE%
 echo "%VERSION%" >> %VERSION_FILE%
 net use X: %DESTINATION_PATH% "%DESTINATION_PWD%" /User:%DESTINATION_USER%
 xcopy Deploy\* X:\%APP_NAME%_%RUN_ID%\* /Y /E
-stop.bat
+.github\scripts\stop.bat
 X:
 move /Y X:\%APP_NAME% X:\Backups\%APP_NAME%_%RUN_ID%
 rename X:\%APP_NAME%_%RUN_ID% %APP_NAME%
 c:
 net use X: /d /Y
-start.bat
+.github\scripts\start.bat
