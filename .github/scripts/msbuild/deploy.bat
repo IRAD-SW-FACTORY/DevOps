32ca38d7f3ff395724e505cb2d6061cf54bb8f9f
Jorge <jorge@jamtech.cl>
2023-09-28 22:32:11 -0300

    refactor

--- .github/scripts/msbuild/deploy.bat
+++ .github/scripts/msbuild/deploy.bat
@@ -1,4 +1,4 @@
-echo "Running deploy.bat"
+echo "Running deploy.bat for repo %REPO_NAME%"
 echo "Publishing of %APP_NAME% started"
 mkdir Deploy
 cd %PROJECT_PATH%%
@@ -7,12 +7,10 @@ echo "%APP_NAME%" > %VERSION_FILE%
 echo "%VERSION%" >> %VERSION_FILE%
 net use X: %DESTINATION_PATH% "%DESTINATION_PWD%" /User:%DESTINATION_USER%
 xcopy Deploy\* X:\%APP_NAME%_%RUN_ID%\* /Y /E
-echo "Stop App Pool %APP_NAME%"
-PsExec64.exe -s -nobanner -accepteula \\%SERVER% -u %DESTINATION_USER% -p "%DESTINATION_PWD%" "cmd /c C:\Windows\System32\inetsrv\appcmd.exe" "stop apppool /apppool.name:%APP_NAME%"
+stop.bat
 X:
 move /Y X:\%APP_NAME% X:\Backups\%APP_NAME%_%RUN_ID%
 rename X:\%APP_NAME%_%RUN_ID% %APP_NAME%
 c:
 net use X: /d /Y
-echo "Start App Pool %APP_NAME%"
-PsExec64.exe -s -nobanner -accepteula \\%SERVER% -u %DESTINATION_USER% -p "%DESTINATION_PWD%" "cmd /c C:\Windows\System32\inetsrv\appcmd.exe" "start apppool /apppool.name:%APP_NAME%"
+start.bat
---
+++ .github/scripts/msbuild/stop.bat
@@ -0,0 +1,2 @@
+echo "Stop App Pool or Service %APP_NAME%"
+PsExec64.exe -s -nobanner -accepteula \\%SERVER% -u %DESTINATION_USER% -p "%DESTINATION_PWD%" "cmd /c C:\Windows\System32\inetsrv\appcmd.exe" "stop apppool /apppool.name:%APP_NAME%"
