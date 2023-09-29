82d207417aeb899103f697c333ec6112724cd137
Jorge <jaguadom@gmail.com>
2023-09-29 17:48:49 -0300

    Fix psexec permissions (#21)

--- .github/scripts/msbuild/deploy.bat
+++ .github/scripts/msbuild/deploy.bat
@@ -7,10 +7,10 @@ echo "%APP_NAME%" > %VERSION_FILE%
 echo "%VERSION%" >> %VERSION_FILE%
 net use X: %DESTINATION_PATH% "%DESTINATION_PWD%" /User:%DESTINATION_USER%
 xcopy Deploy\* X:\%APP_NAME%_%RUN_ID%\* /Y /E
-.github\scripts\stop.bat
+call .github\scripts\stop.bat
 X:
 move /Y X:\%APP_NAME% X:\Backups\%APP_NAME%_%RUN_ID%
 rename X:\%APP_NAME%_%RUN_ID% %APP_NAME%
 c:
 net use X: /d /Y
-.github\scripts\start.bat
+call .github\scripts\start.bat
