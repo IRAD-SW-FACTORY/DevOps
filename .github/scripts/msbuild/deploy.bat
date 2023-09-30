d47685e5f2c2c62b234dbaf583c4fe89d13daa47
Jorge <jaguadom@gmail.com>
2023-09-30 18:07:49 -0300

    Fix deployment path with spaces (#24)

--- .github/scripts/msbuild/deploy.bat
+++ .github/scripts/msbuild/deploy.bat
@@ -13,11 +13,11 @@ IF "%KIND%" == "AppWeb" (
 echo "%APP_NAME%" > %VERSION_FILE%
 echo "%VERSION%" >> %VERSION_FILE%
 net use X: %DESTINATION_PATH% "%DESTINATION_PWD%" /User:%DESTINATION_USER%
-xcopy Deploy\* X:\%APP_NAME%_%RUN_ID%\* /Y /E
+xcopy Deploy\* "X:\%APP_NAME%_%RUN_ID%\*" /Y /E
 call .github\scripts\stop.bat
 X:
-move /Y X:\%APP_NAME% X:\Backups\%APP_NAME%_%RUN_ID%
-rename X:\%APP_NAME%_%RUN_ID% %APP_NAME%
+move /Y "X:\%APP_NAME%" "X:\Backups\%APP_NAME%_%RUN_ID%"
+rename "X:\%APP_NAME%_%RUN_ID%" "%APP_NAME%"
 c:
 net use X: /d /Y
 call .github\scripts\start.bat
