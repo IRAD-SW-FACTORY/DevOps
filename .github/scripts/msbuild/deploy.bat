6b3d5e0792fa977e4ad25f8dfb6180c60084b33a
Jorge <jaguadom@gmail.com>
2023-09-30 18:01:45 -0300

    Fix service deployment (#23)

--- .github/scripts/msbuild/deploy.bat
+++ .github/scripts/msbuild/deploy.bat
@@ -2,7 +2,14 @@ echo "Running deploy.bat for repo %REPO_NAME%"
 echo "Publishing of %APP_NAME% started"
 mkdir Deploy
 cd %PROJECT_PATH%%
-msbuild /p:DeployOnBuild=true /p:PublishProfile=DeployFolder /p:Configuration=Release /p:Platform="Any CPU"
+IF "%KIND%" == "WinService" (
+    msbuild  /p:Configuration=Release /p:Platform="Any CPU"
+    xcopy bin\Release\* ..\Deploy\* /Y /E
+)
+IF "%KIND%" == "AppWeb" (
+  msbuild /p:DeployOnBuild=true /p:PublishProfile=DeployFolder /p:Configuration=Release /p:Platform="Any CPU"
+)
+
 echo "%APP_NAME%" > %VERSION_FILE%
 echo "%VERSION%" >> %VERSION_FILE%
 net use X: %DESTINATION_PATH% "%DESTINATION_PWD%" /User:%DESTINATION_USER%
