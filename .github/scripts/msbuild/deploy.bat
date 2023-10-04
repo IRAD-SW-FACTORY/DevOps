83d10dba49951171ad5e1a148e9f752045ae9444
Jorge <jaguadom@gmail.com>
2023-10-04 20:22:19 -0300

    Fix deploy api (#47)

--- .github/scripts/msbuild/deploy.bat
+++ .github/scripts/msbuild/deploy.bat
@@ -18,8 +18,8 @@ net use X: %DESTINATION_PATH% "%DESTINATION_PWD%" /User:%DESTINATION_USER%
 xcopy Deploy\* "X:\%APP_NAME%_%RUN_ID%\*" /Y /E
 call .github\scripts\stop.bat
 X:
-move /Y "X:\%APP_NAME%\*" "X:\Backups\%APP_NAME%_%RUN_ID%\"
-move /Y "X:\%APP_NAME%_%RUN_ID%\*" "%APP_NAME%\"
+xcopy "X:\%APP_NAME%\*" "X:\Backups\%APP_NAME%_%RUN_ID%\*"  /Y /E
+xcopy "X:\%APP_NAME%_%RUN_ID%\*" "%APP_NAME%\*"  /Y /E
 rmdir "X:\%APP_NAME%_%RUN_ID%" /Q/S
 c:
 net use X: /d /Y
