fe70b80e6e1fd198524c0264d2c128628b49fa80
Jorge <jorge@jamtech.cl>
2023-10-07 01:01:38 -0300

    fix

--- .github/scripts/msbuild/release.bat
+++ .github/scripts/msbuild/release.bat
@@ -15,7 +15,5 @@ echo "%VERSION%" >> %VERSION_FILE%
 powershell Compress-Archive Deploy Release.zip
 
 net use X: %DESTINATION_PATH% "%DESTINATION_PWD%" /User:%DESTINATION_USER%
-X:
 xcopy Release.zip "X:\Releases\%APP_NAME%_%RUN_ID%.zip"  /Y /E
-c:
 net use X: /d /Y
