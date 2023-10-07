65449b53c5a9816d15b35e6e22234214aba01f2a
Jorge <jorge@jamtech.cl>
2023-10-07 03:17:03 -0300

    add win service logic

--- src/WebDeployApi/Logic/WinService.cs
+++ src/WebDeployApi/Logic/WinService.cs
@@ -1,14 +1,30 @@
-﻿namespace WebDeployApi.Logic
+﻿using System.Diagnostics;
+
+namespace WebDeployApi.Logic
 {
     public class WinService
     {
         public static void Start(string name)
         {
-
+            Process p = new Process();
+            p.StartInfo.FileName = "C:\\Windows\\system32\\sc.exe";
+            p.StartInfo.Arguments = $"start {name}";
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.WaitForExit();
+            if (p.ExitCode != 0)
+                throw new System.Exception($"Error starting service {name}");
         }
         public static void Stop(string name)
         {
-
+            Process p = new Process();
+            p.StartInfo.FileName = "C:\\Windows\\system32\\sc.exe";
+            p.StartInfo.Arguments = $"stop {name}";
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.WaitForExit();
+            if (p.ExitCode != 0)
+                throw new System.Exception($"Error starting service {name}");
         }
     }
 }
