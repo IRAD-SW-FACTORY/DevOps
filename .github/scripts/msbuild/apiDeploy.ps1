6de7029dd9a7715a6df909eff45841edb3e1ba07
Jorge <jorge@jamtech.cl>
2023-10-13 18:27:02 -0300

    fix

--- .github/scripts/msbuild/apiDeploy.ps1
+++ .github/scripts/msbuild/apiDeploy.ps1
@@ -1,12 +1,13 @@
 Write-Output "Deploy started using API"
-$destinationServerBaseUrl = if ($null -ne $env:SERVER) { "http://${env:SERVER}" } else { "http://ec2-54-152-53-114.compute-1.amazonaws.com" };
-$releasesServerBaseUrl = if ($null -ne $env:SERVER) { "http://${env:SERVER}" } else { "http://ec2-54-152-53-114.compute-1.amazonaws.com" };
-$runId = if ($null -ne $env:RUN_ID) { $env:RUN_ID } else { "6439638128" };
-$kind = if ($null -ne $env:KIND) { $env:KIND == "AppWeb" ? 0 : 1 } else { 0 };
-$name = if ($null -ne $env:APP_NAME) { $env:APP_NAME } else { "MultiRisWeb" };
+$destinationServerBaseUrl = if ($null -ne $env:SERVER) { "http://${env:SERVER}" } else { "http://ec2-54-152-53-114.compute-1.amazonaws.com" }
+$releasesServerBaseUrl = if ($null -ne $env:SERVER) { "http://${env:SERVER}" } else { "http://ec2-54-152-53-114.compute-1.amazonaws.com" }
+$runId = if ($null -ne $env:RUN_ID) { $env:RUN_ID } else { "6439638128" }
+$kind = if ($null -ne $env:KIND) { if ($env:KIND -eq "WinService") { 1 } else { 0 } } else { 0 }
+$name = if ($null -ne $env:APP_NAME) { $env:APP_NAME } else { "MultiRisWeb" }
+$serviceName = if ($null -ne $env:SERVICE_NAME) { $env:SERVICE_NAME } else { "" }
 $user = if ($null -ne $env:DESTINATION_USER) { $env:DESTINATION_USER } else { "devops" };
-$pass = if ($null -ne $env:DESTINATION_PWD) { $env:DESTINATION_PWD } else { "Oh$&OGfK@-2K=9diZF8Cd!k0WK.ZU)Bl" };
-$localPath = if ($null -ne $env:DESTINATION_LOCAL_PATH) { $env:DESTINATION_LOCAL_PATH } else { "c:\\Sitios" };
+$pass = if ($null -ne $env:DESTINATION_PWD) { $env:DESTINATION_PWD } else { "Oh$&OGfK@-2K=9diZF8Cd!k0WK.ZU)Bl" }
+$localPath = if ($null -ne $env:DESTINATION_LOCAL_PATH) { $env:DESTINATION_LOCAL_PATH } else { "c:\\Sitios" }
 
 $authToken = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("${user}:${pass}"))
 $releasedPackage = "${name}_${runId}.zip"
@@ -23,6 +24,7 @@ $body = @"
 {
     "kind": $kind,
     "name": "$name",
+    "serviceName":  "$serviceName",
     "deploymentUrl": "$releaseUrl",
     "deploymentLocalPath": "$localPath",
     "backupLocalPath": "$backupsPath",
