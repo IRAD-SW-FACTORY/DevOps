22f102a956f3eb02e9f9f570629301fab584e336
Jorge <jorge@jamtech.cl>
2023-10-13 18:42:04 -0300

    fix releases path

--- .github/scripts/msbuild/apiDeploy.ps1
+++ .github/scripts/msbuild/apiDeploy.ps1
@@ -3,6 +3,7 @@ $destinationServerBaseUrl = if ($null -ne $env:SERVER) { "http://${env:SERVER}" 
 $releasesServerBaseUrl = if ($null -ne $env:SERVER) { "http://${env:SERVER}" } else { "http://ec2-54-152-53-114.compute-1.amazonaws.com" }
 $runId = if ($null -ne $env:RUN_ID) { $env:RUN_ID } else { "6439638128" }
 $kind = if ($null -ne $env:KIND) { if ($env:KIND -eq "WinService") { 1 } else { 0 } } else { 0 }
+$releaeType = if ($kind -eq 0) { "Web" } else { "Service" } 
 $name = if ($null -ne $env:APP_NAME) { $env:APP_NAME } else { "MultiRisWeb" }
 $serviceName = if ($null -ne $env:SERVICE_NAME) { $env:SERVICE_NAME } else { "" }
 $user = if ($null -ne $env:DESTINATION_USER) { $env:DESTINATION_USER } else { "devops" };
@@ -13,7 +14,7 @@ $authToken = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("${
 $releasedPackage = "${name}_${runId}.zip"
 $backupsPath =  "${localPath}\\Backups"
 $apiUrl = "$destinationServerBaseUrl/DeployApi/api/Deploy"
-$releaseUrl = "$releasesServerBaseUrl/WebReleases/$releasedPackage"
+$releaseUrl = "$releasesServerBaseUrl/${$releaeType}Releases/$releasedPackage"
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Accept", "application/json")
