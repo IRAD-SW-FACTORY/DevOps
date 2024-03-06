b7c15f73d616a11a41dd11078e85ca2acd0e3936
Jorge <jaguadom@gmail.com>
2024-03-06 14:50:31 -0300

    exclude .config on folder deploy

--- .github/scripts/msbuild/folderdeploy.bat
+++ .github/scripts/msbuild/folderdeploy.bat
@@ -1,6 +1,6 @@
 echo "Running deploy.bat for repo %REPO_NAME%"
 net use X: %DESTINATION_PATH% "%DESTINATION_PWD%" /User:%DESTINATION_USER%
-xcopy Deploy\* "X:\%APP_NAME%_%RUN_ID%\*" /Y /E
+xcopy Deploy\* "X:\%APP_NAME%_%RUN_ID%\*" /Y /E /EXCLUDE:.config
 call .github\scripts\stop.bat
 X:
 xcopy "X:\%APP_NAME%\*" "X:\Backups\%APP_NAME%_%RUN_ID%\*"  /Y /E
