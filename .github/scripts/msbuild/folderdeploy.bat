28c9a1df35f132dc1b4d22e3e4e55d64636ea02e
Jorge <jaguadom@gmail.com>
2024-07-04 04:20:11 -0400

    Fix .config replacing (#73)

--- .github/scripts/msbuild/folderdeploy.bat
+++ .github/scripts/msbuild/folderdeploy.bat
@@ -1,13 +1,14 @@
 echo "Running deploy.bat for repo %REPO_NAME%"
 net use X: %DESTINATION_PATH% "%DESTINATION_PWD%" /User:%DESTINATION_USER%
-xcopy Deploy\* "X:\%APP_NAME%_%RUN_ID%\*" /Y /E /EXCLUDE:.config
+xcopy Deploy\* "X:\%APP_NAME%_%RUN_ID%\*" /Y /E
 call .github\scripts\stop.bat
 X:
 xcopy "X:\%APP_NAME%\*" "X:\Backups\%APP_NAME%_%RUN_ID%\*"  /Y /E
 xcopy "X:\%APP_NAME%_%RUN_ID%\*" "%APP_NAME%\*"  /Y /E
-echo "Copy configs from backup"
-xcopy "X:\Backups\%APP_NAME%_%RUN_ID%\*.config" "X:\%APP_NAME%\*"  /Y /E /C
+echo "Copy configs from backup new"
+xcopy "X:\Backups\%APP_NAME%_%RUN_ID%\*.config" "X:\%APP_NAME%\"  /Y /E /C
 rmdir "X:\%APP_NAME%_%RUN_ID%" /Q /S
 c:
 net use X: /d /Y
 call .github\scripts\start.bat
+
